6e384a7b74a17007a69006d3c6bcf0f4531ad059
Raman Gupta <7243222+raman325@users.noreply.github.com>
2022-01-02 20:23:45 -0500

    Fix issue with tracking multiple accounts

--- custom_components/zoom/common.py
+++ custom_components/zoom/common.py
@@ -113,8 +113,8 @@ class ZoomWebhookRequestView(HomeAssistantView):
                 try:
                     data = await request.json()
                     status = WEBHOOK_RESPONSE_SCHEMA(data)
-                    _LOGGER.debug("Received event: %s", status)
-                    hass.bus.async_fire(f"{HA_ZOOM_EVENT}", {**status, "token": token})
+                    status["verification_token"] = token
+                    hass.bus.async_fire(f"{HA_ZOOM_EVENT}", status)
                 except Exception as err:
                     _LOGGER.warning(
                         "Received authorized event but unable to parse: %s (%s)",
